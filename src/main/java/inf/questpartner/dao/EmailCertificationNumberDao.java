dfbb93eeb80abff7b67ef25e3d3f0f8e9623bc53
Lanvizu <ghzm888@gmail.com>
2024-02-03 20:42:54 +0900

    feat: Email 관련 Dao 추가

---
+++ src/main/java/inf/questpartner/dao/EmailCertificationNumberDao.java
@@ -0,0 +1,38 @@
+package inf.questpartner.dao;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.time.Duration;
+
+import static inf.questpartner.common.utils.EmailConstants.LIMIT_TIME_CERTIFICATION_NUMBER;
+import static inf.questpartner.common.utils.EmailConstants.PREFIX_CERTIFICATION;
+
+@RequiredArgsConstructor
+@Repository
+public class EmailCertificationNumberDao implements EmailCertificationDao {
+    private final StringRedisTemplate redisTemplate;
+
+    @Override
+    public void createEmail(String email, String certificationNumber) {
+        redisTemplate.opsForValue()
+                .set(PREFIX_CERTIFICATION + email, certificationNumber,
+                        Duration.ofSeconds(LIMIT_TIME_CERTIFICATION_NUMBER));
+    }
+
+    @Override
+    public String getEmailCertification(String email) {
+        return redisTemplate.opsForValue().get(PREFIX_CERTIFICATION + email);
+    }
+
+    @Override
+    public void removeEmailCertification(String email) {
+        redisTemplate.delete(PREFIX_CERTIFICATION + email);
+    }
+
+    @Override
+    public boolean hasKey(String email) {
+        return redisTemplate.hasKey(PREFIX_CERTIFICATION + email);
+    }
+}

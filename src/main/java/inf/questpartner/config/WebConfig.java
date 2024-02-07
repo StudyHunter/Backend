c8659ffbf14596d48691464e75e5321c69e3f528
Junho Lee <ghzm888@gmail.com>
2024-02-07 12:57:00 +0900

    feat: WebConfig와 Interceptor 추가

--- src/main/java/inf/questpartner/config/WebConfig.java
+++ src/main/java/inf/questpartner/config/WebConfig.java
@@ -1,6 +1,30 @@
 package inf.questpartner.config;
+
+import inf.questpartner.util.validation.interceptor.LoginCheckInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
 /*
  예시
  */
-public class WebConfig {
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginCheckInterceptor loginCheckInterceptor;
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginCheckInterceptor);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserArgumentResolver);
+    }
 }
--- src/main/java/inf/questpartner/util/validation/interceptor/LoginCheckInterceptor.java
+++ src/main/java/inf/questpartner/util/validation/interceptor/LoginCheckInterceptor.java
@@ -1,7 +1,89 @@
 package inf.questpartner.util.validation.interceptor;
 
+import inf.questpartner.common.annotation.LoginCheck;
+import inf.questpartner.domain.users.common.UserLevel;
+import inf.questpartner.service.SessionLoginService;
+import inf.questpartner.util.exception.users.NotAuthorizedException;
+import inf.questpartner.util.exception.users.UnauthenticatedUserException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
 /**
  * 예시
  */
-public class LoginCheckInterceptor {
+@Component
+@RequiredArgsConstructor
+public class LoginCheckInterceptor implements HandlerInterceptor {
+
+    private final SessionLoginService sessionLoginService;
+
+    @Autowired
+//    @inject 변경
+    private Environment environment;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+                             Object handler)
+            throws Exception {
+
+        // test 환경에서는 인터셉트가 동작하지 않도록 설정
+        String[] activeProfiles = environment.getActiveProfiles();
+        if ("test".equals(activeProfiles[0])) {
+            return true;
+        }
+
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod handlerMethod = (HandlerMethod) handler;
+            LoginCheck loginCheck = handlerMethod.getMethodAnnotation(LoginCheck.class);
+
+            if (loginCheck == null) {
+                return true;
+            }
+
+            if (sessionLoginService.getLoginUser() == null) {
+                throw new UnauthenticatedUserException("로그인 후 이용 가능합니다.");
+            }
+
+            UserLevel auth = loginCheck.authority();
+
+
+            switch (auth) {
+                case ADMIN:
+                    adminUserLevel();
+                    break;
+
+                case AUTH:
+                    authUserLevel();
+                    break;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 현재 USER의 권한(UserLevel)이 AUTH인지 확인한다. 해당 리소스는 ADMIN과 AUTH만 접근 가능하다. 따라서 UNAUTH인 경우에만 제한한다.
+     */
+    private void authUserLevel() {
+        UserLevel auth = sessionLoginService.getUserLevel();
+        if (auth == UserLevel.UNAUTH) {
+            throw new NotAuthorizedException("해당 리소스에 대한 접근 권한이 존재하지 않습니다.");
+
+        }
+    }
+
+    /**
+     * 현재 USER의 권한(UserLevel)이 ADMIN인지 확인한다. ADMIN이 아니라면 해당 요청을 수행할 수 없다.
+     */
+    private void adminUserLevel() {
+        UserLevel auth = sessionLoginService.getUserLevel();
+        if (auth != UserLevel.ADMIN) {
+            throw new NotAuthorizedException("해당 리소스에 대한 접근 권한이 존재하지 않습니다.");
+        }
+    }
 }

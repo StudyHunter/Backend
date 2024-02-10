c4b62b7e272a00f2f2eb28a97ea1e72d6f148c5a
Lanvizu <ghzm888@gmail.com>
2024-02-10 18:11:11 +0900

    style: 사소한 부분 제거

--- src/main/java/inf/questpartner/common/properties/AppProperties.java
+++ src/main/java/inf/questpartner/common/properties/AppProperties.java
@@ -1,13 +1,8 @@
 package inf.questpartner.common.properties;
 
-import lombok.Data;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 @Getter
 @Configuration

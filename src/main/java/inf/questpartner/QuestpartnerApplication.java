40d2a60fee33a445ff3066eb00548af9aca21d4b
Lanvizu <ghzm888@gmail.com>
2024-02-10 18:04:28 +0900

    fix: merge 시 발생하는 AppProperties 수정

--- src/main/java/inf/questpartner/QuestpartnerApplication.java
+++ src/main/java/inf/questpartner/QuestpartnerApplication.java
@@ -1,14 +1,10 @@
 package inf.questpartner;
 
-import inf.questpartner.common.properties.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(value = {AppProperties.class})
 public class QuestpartnerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(QuestpartnerApplication.class, args);
 	}
--- src/main/java/inf/questpartner/common/properties/AppProperties.java
+++ src/main/java/inf/questpartner/common/properties/AppProperties.java
@@ -1,18 +1,22 @@
 package inf.questpartner.common.properties;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Data
-@RequiredArgsConstructor
-//@Configuration
-//@ConstructorBinding
-@ConfigurationProperties("external-certification")
+@Getter
+@Configuration
 public class AppProperties {
 
     private final String emailFromAddress;
-//    private final String coolSmsKey;
-//    private final String coolSmsSecret;
-//    private final String coolSmsFromPhoneNumber;
+
+//    이메일 주소 단일값만 가져오므로 Value로 수정.
+    public AppProperties(@Value("${external-certification.email-from-address}") String emailFromAddress) {
+        this.emailFromAddress = emailFromAddress;
+    }
 }
